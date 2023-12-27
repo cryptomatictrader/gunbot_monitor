2eb87711808fac2485292a08ee6facc4b0e5be2c
Cryptomatic Trader <cryptomatictrader@proton.me>
2023-12-27 15:10:01 +0800

    Cut TG message length to max size allowed

--- helpers/telegram_client.js
+++ helpers/telegram_client.js
@@ -1,12 +1,13 @@
 const TelegramBot = require("node-telegram-bot-api");
 const config = require("config");
+const MAX_MESSAGE_SIZE = 4096;
 
 class TelegramClient {
     constructor() {
         this.bot = new TelegramBot(config.get("TG_BOT_TOKEN")); // polling false by default
     }
     sendMessage(msg) {
-        this.bot.sendMessage(config.get("TG_CHAT_ID"), msg);
+        this.bot.sendMessage(config.get("TG_CHAT_ID"), msg.substring(0, MAX_MESSAGE_SIZE));
     }
 }
 
